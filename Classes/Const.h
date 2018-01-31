2b17e43ea20d0865992aefa028c28947f7becbec
Brj <153065642@qq.com>
2018-01-31 10:58:18 +0800

    计费点修改

--- Classes/Const.h
+++ Classes/Const.h
@@ -1,4 +1,4 @@
-﻿
+
 #ifndef _H_CONST_H_
 #define _H_CONST_H_
 #include "cocos2d.h"
@@ -61,8 +61,8 @@ static std::string heroname[] = { "小虾米", "三少爷", "江小白", "阿青
 static std::string herodesc[] = { "名门之后，因家族武功被邪教窥觑，惨遭灭门，只剩孤身一人", "从未来穿越而来的特种兵，为了回到属于自己的时代而被迫行走江湖", "恶人谷出身，习得各大恶人的看家本领，但本性善良，被送出谷历练", "情郎被邪教蛊惑，为寻回情郎，孤身一人闯荡江湖！" };
 static int heroprice[] = { 0, 6, 12, 30 };
 
-static std::string payCode[] = { "", "wx010", "wx011", "wx013", "wx018", "wx020", "wx021", "wx026", "wx022", "wx023","wx024","wx025"};
-static int goldcount[] = {60,140, 400, 1080, 2388};
+static std::string payCode[] = { "", "bwx010", "bwx011", "bwx013", "bwx018", "bwx019", "bwx020", "bwx021", "bwx026", "wx022", "wx023","wx024","wx025"};
+static int goldcount[] = {60, 140, 400, 1080, 2388};
 static int vipgoldcount[] = { 10,20, 50 };
 
 static int buyprice[] = { 0, 6, 12, 30, 6,12, 30, 68, 128, 6, 30, 68, 18 };
